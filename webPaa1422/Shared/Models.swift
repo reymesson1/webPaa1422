1f553fcb5a63d0cbb569c16fb15297e048b47658
reymesson1 <rey_messon@hotmail.com>
2021-08-22 09:10:37 -0400

    done

---
+++ webPaa1422/Shared/Models.swift
@@ -0,0 +1,24 @@
+//
+//  Models.swift
+//  webPaa1422
+//
+//  Created by MacBook Pro on 8/22/21.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct DataModel {
+    let error: String
+    let message: String
+    let data: []
+}
+
+struct PostModel : Decodable {
+    
+    let id : Int
+    let title: String
+    let
+    
+}
---
+++ webPaa1422/Shared/webPaa1422App.swift
@@ -0,0 +1,17 @@
+//
+//  webPaa1422App.swift
+//  Shared
+//
+//  Created by MacBook Pro on 8/22/21.
+//
+
+import SwiftUI
+
+@main
+struct webPaa1422App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
