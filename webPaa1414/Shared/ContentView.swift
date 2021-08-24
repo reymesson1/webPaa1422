692fec58998a49f9907372c1a97e1f7963abcaa2
reymesson1 <rey_messon@hotmail.com>
2021-08-24 11:07:43 -0400

    done

--- webPaa1414/Shared/ContentView.swift
+++ webPaa1414/Shared/ContentView.swift
@@ -8,51 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentState: Bool = true
 
     var body: some View {
-
-        HomeView()
         
+        NavigationView{
+                    
+            VStack {
+                newHeader()
+                    .frame(height:300)
+                
+                VStack{
+                    
+                    Toggle(isOn: $currentState, label:{
+                        Text(currentState ? "On" : "Off")
+                            .padding()
+                    }).onChange(of: self.currentState, perform: { value in
+                        print("Value has changed : \(value)")
+                    })
+
+                    
+                    HStack{
+                        NavigationLink("Filter", destination: EmptyView()).padding()
+                        Spacer()
+                    }
+
+                    HomeView()
+                        .frame(height:700)
+                    
+                }
+
+            }
+        }.navigationViewStyle(StackNavigationViewStyle())
         
     }
 }
 
-struct HomeView: View {
-    @EnvironmentObject var viewModel: ViewModel
-
+struct newHeader: View {
     var body: some View {
 
-        NavigationView{
-            
-            List{
-                
-                ForEach(viewModel.items, id: \.id){ item in
-//                    ForEach(0 ..< 5, id: \.self){ item in
-
-                    Text("Hello, world!")
-                        .padding()
-
+        ZStack{
+            Color(red: 12 / 255, green: 53 / 255, blue: 62 / 255)
+            HStack{
+                Group{
+                    VStack{
+                        Text("Amsel")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                        Text("eCatalog")
+                            .foregroundColor(.orange)
+                    }
                 }
+                Spacer()
+                Group{
+                    VStack{
+                        Spacer()
+                        NavigationLink(" ", destination: EmptyView())
+                        NavigationLink("", destination: EmptyView())
+                        NavigationLink(" ", destination: EmptyView())
+                        NavigationLink("", destination: EmptyView())
+                        NavigationLink("Browse", destination: EmptyView())
+                        Spacer()
+                    }
+                }
+                Spacer()
+                Group{
+                    VStack{
+                        Spacer()
+                        NavigationLink(" ", destination: EmptyView())
+                        NavigationLink("", destination: EmptyView())
+                        NavigationLink(" ", destination: EmptyView())
+                        NavigationLink("", destination: EmptyView())
+                        NavigationLink("Product", destination: EmptyView())
+                        Spacer()
+                    }
+                }
+                Spacer()
+                Group{
+                    VStack{
+                        Spacer()
+                        NavigationLink(" ", destination: EmptyView())
+                        NavigationLink("", destination: EmptyView())
+                        NavigationLink(" ", destination: EmptyView())
+                        NavigationLink("", destination: EmptyView())
+                        NavigationLink("Style", destination: EmptyView())
+                        Spacer()
+                    }
+                }
+                Spacer()
+                Group{
+                    VStack{
+                        Spacer()
+                        NavigationLink(" ", destination: EmptyView())
+                        NavigationLink("", destination: EmptyView())
+                        NavigationLink(" ", destination: EmptyView())
+                        NavigationLink("", destination: EmptyView())
+                        NavigationLink("Company", destination: EmptyView())
+                        Spacer()
+                    }
+                }
+                Spacer()
             }
-            
-            .navigationBarTitle("Posts")
-            .navigationBarItems(trailing: plusButton)
-            
-            
-        }.navigationViewStyle(StackNavigationViewStyle())
-        
+        }
+
     }
-    
-    var plusButton: some View {
-        
-        Button(action:{
-            print("press")
-        }, label:{
-            Image(systemName: "plus")
-        })
-    
-    }
-    
     
 }
 
---
+++ webPaa1414/Shared/HomeView.swift
@@ -0,0 +1,53 @@
+//
+//  HomeView.swift
+//  webPaa1414
+//
+//  Created by MacBook Pro on 8/24/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var viewModel: ViewModel
+
+    var body: some View {
+
+        NavigationView{
+            
+            List{
+                
+                ForEach(viewModel.items, id: \.id){ item in
+//                    ForEach(0 ..< 5, id: \.self){ item in
+
+                    Text("Hello, world!")
+                        .padding()
+
+                }
+            }
+            
+            .navigationBarTitle("Posts")
+            .navigationBarItems(trailing: plusButton)
+            
+            
+        }.navigationViewStyle(StackNavigationViewStyle())
+        
+    }
+    
+    var plusButton: some View {
+        
+        Button(action:{
+            print("press")
+        }, label:{
+            Image(systemName: "plus")
+        })
+    
+    }
+    
+    
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
